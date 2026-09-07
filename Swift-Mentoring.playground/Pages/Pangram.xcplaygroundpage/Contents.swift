aff2129d31cc86d6b4ef9e794da0e3208c744acc
s78k90 <sumit78kumar90@gmail.com>
2026-09-07 14:07:07 +0530

    Pangram in Swift

---
+++ Swift-Mentoring.playground/Pages/Pangram.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Pangram (English) - a string containing every letter (case-insensitive) of the alphabet at least once.
+// Example: The quick brown fox jumps over the lazy dog.
+
+extension String {
+    func isPangram() -> Bool {
+        guard count >= 26 else { return false }
+        
+        let text = self.lowercased().filter { $0 >= "a" && $0 <= "z" }
+        
+        // Dictionary (key, value)
+//        var dictionary: [Character: Bool] = [:]
+//        for character in text {
+//            dictionary[character] = true
+//        }
+//        
+//        return dictionary.keys.count == 26
+        
+        // Array
+//        var array = Array(repeating: false, count: 26)
+//        for character in text {
+//            let index = Int((character.asciiValue ?? 0) - (Character("a").asciiValue ?? 0))
+//            array[index] = true
+//        }
+//        
+//        return array.allSatisfy { $0 == true }
+        
+        // Set
+        return Set(text).count == 26
+    }
+}
+
+"The quick brown fox jumps over the lazy dog.".isPangram()
+
+//: [Next](@next)
